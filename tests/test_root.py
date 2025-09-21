87264206d87f09380ce496bea275b19cda122c6f
Matthew Fitzpatrick <mrfitzpa@uvic.ca>
2025-09-21 04:35:04 +0000

    First commit. All the source code has been written but has yet to be tested and debugged.

---
+++ tests/test_root.py
@@ -0,0 +1,151 @@
+# -*- coding: utf-8 -*-
+# Copyright 2025 Matthew Fitzpatrick.
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <https://www.gnu.org/licenses/gpl-3.0.html>.
+r"""Contains tests for the root of the package :mod:`pylibtemplate`.
+
+"""
+
+
+
+#####################################
+## Load libraries/packages/modules ##
+#####################################
+
+# For operations related to unit tests.
+import pytest
+
+# For removing directories.
+import shutil
+
+# For setting file permissions.
+import os
+
+# For making directories.
+import pathlib
+
+
+
+# For generating local ``git`` repository templates.
+import pylibtemplate
+
+
+
+##################################
+## Define classes and functions ##
+##################################
+
+def test_1_of_generate_local_git_repo_template():
+    path_to_test_data = "./pylibtemplate"
+
+    generate_local_git_repo_template()
+    
+    shutil.rmtree(path_to_test_data)
+
+    return None
+
+
+
+def test_2_of_generate_local_git_repo_template():
+    path_to_test_data = "./test_data"
+
+    kwargs = {"lib_name_for_imports": \
+              "mypylib",
+              "abbreviated_lib_name_for_docs": \
+              "MyPyLib",
+              "non_abbreviated_lib_name_for_docs": \
+              "My Python Library",
+              "author": \
+              "Randy Lahey",
+              "email": \
+              "randy.lahey@bobandy.com",
+              "gist_id": \
+              "5klmds090sdm2jansdu92nrlkjnmsa9r",
+              "path_to_directory_to_contain_new_repo": \
+              path_to_test_data + "/mypylib"}
+    generate_local_git_repo_template(**kwargs)
+
+    shutil.rmtree(path_to_test_data)
+
+    return None
+
+
+
+def test_2_of_generate_local_git_repo_template():
+    path_to_test_data = "./test_data"
+    pathlib.Path(path_to_test_data).mkdir(parents=True, exist_ok=True)
+    os.chmod(path_to_test_data, 0o111)
+
+    with pytest.raises(IOError) as err_info:
+        kwargs = {"path_to_directory_to_contain_new_repo": \
+                  path_to_test_data + "/mypylib"}
+        generate_local_git_repo_template(**kwargs)
+
+    os.chmod(path_to_test_data, 0o711)
+
+    shutil.rmtree(path_to_test_data)
+
+    return None
+
+
+
+def test_3_of_generate_local_git_repo_template():
+    with pytest.raises(ValueError) as err_info:
+        kwargs = {"lib_name_for_imports": "123abc"}
+        generate_local_git_repo_template(**kwargs)
+
+    with pytest.raises(ValueError) as err_info:
+        kwargs = {"abbreviated_lib_name_for_docs": "123abc"}
+        generate_local_git_repo_template(**kwargs)
+
+    param_name_subset = ("author",
+                         "email",
+                         "gist_id",
+                         "path_to_directory_to_contain_new_repo")
+
+    for param_name in param_name_subset:
+        with pytest.raises(TypeError) as err_info:
+            kwargs = {param_name: slice(None)}
+            generate_local_git_repo_template(**kwargs)
+
+    return None
+
+
+
+def test_4_of_generate_local_git_repo_template():
+    path_to_test_data = "./test_data"
+
+    kwargs = {"lib_name_for_imports": \
+              "mypylib",
+              "abbreviated_lib_name_for_docs": \
+              "MyPyLib",
+              "non_abbreviated_lib_name_for_docs": \
+              "My Python Library",
+              "author": \
+              "F"+ "o"*130 +" Bar",
+              "email": \
+              "randy.lahey@bobandy.com",
+              "gist_id": \
+              "5klmds090sdm2jansdu92nrlkjnmsa9r",
+              "path_to_directory_to_contain_new_repo": \
+              path_to_test_data + "/mypylib"}
+    generate_local_git_repo_template(**kwargs)
+
+    shutil.rmtree(path_to_test_data)
+
+    return None
+
+
+
+###########################
+## Define error messages ##
+###########################
